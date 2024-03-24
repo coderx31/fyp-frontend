5e78e8300531ff25718e8c1115631a1940d82754
= <=>
2024-03-24 18:38:50 +0530

    a tag link issue fixed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,20 +4,13 @@ import './Navigation.css'
 
 const Navigation = () => {
   return (
-    // <nav>
-    //     <ul>
-    //         <li><Link to="/">Home</Link></li>
-    //         <li><Link to="/about">About</Link></li>
-    //         <li><Link to="/faq">FAQ</Link></li>
-    //     </ul>
-    // </nav>
     <header>
         <a href="#" className='logo'>Hullooo</a>
         <ul>
-        <li><a href="#"><Link to="/">Home</Link></a></li>
-        <li><a href="#"><Link to="/about">About</Link></a></li>
-        <li><a href="#"><Link to="/faq">FAQ</Link></a></li>
-        <li><a href="#"><Link to="/feedback">Feedback</Link></a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/faq">FAQ</Link></li>
+        <li><Link to="/feedback">Feedback</Link></li>
         </ul>
     </header>
   )
